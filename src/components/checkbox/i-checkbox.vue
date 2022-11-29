96fe474c8b2b7d724002daff43293735ffc24b0d
Bob Tang <254202042@qq.com>
2022-11-29 19:56:53 +0800

    feat:checkbox

--- src/components/checkbox/i-checkbox.vue
+++ src/components/checkbox/i-checkbox.vue
@@ -1,18 +1,28 @@
 <template>
     <label>
         <span>
+            <input
+                v-if="group"
+                type="checkbox"
+                :disabled="disabled"
+                :value="label"
+                v-model="model"
+                @change="change"
+            />
             <input 
+                v-else
                 type="checkbox"
                 :disabled="disabled"
                 :checked="currentValue"
                 @change="change"
-            >
+            />
         </span>
     </label>
 </template>
 
 <script>
 import Emitter from '../../mixins/emitter'
+import { findComponentUpward } from '@/utils/assist'
 export default {
     name:"iCheckbox",
     mixins:[Emitter],
@@ -48,6 +58,15 @@ export default {
             }
         }
     },
+    mounted() {
+        this.parent = findComponentUpward(this,'iCheckboxGroup')
+
+        if(this.parent) {
+            this.group = true;
+        } else {
+            this.updateModel();
+        }
+    },
     methods: {
         change(event) {
             if(this.disabled) {
@@ -57,8 +76,12 @@ export default {
             this.currentValue = checked;
             
             const value = checked ? this.trueValue : this.falseValue;this.$emit('input', value);
-            this.$emit('on-change', value);
-            this.dispatch('iFormItem','on-form-change',value)
+            if (this.group) {
+                this.parent.change(this.model);
+            } else {
+                this.$emit('on-change', value);
+                this.dispatch('iFormItem', 'on-form-change', value);
+            }
         },
         updateModel() {
             this.currentValue = this.value === this.trueValue
